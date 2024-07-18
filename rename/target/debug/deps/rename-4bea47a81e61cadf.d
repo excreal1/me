214623f83a6fbe0410f9ad3c604142a9305f15ac
excreal1 <148652451+excreal1@users.noreply.github.com>
2024-07-18 20:24:26 +0000

    Updates

---
+++ rename/target/debug/deps/rename-4bea47a81e61cadf.d
@@ -0,0 +1,5 @@
+/home/merlin/github/me/rename/target/debug/deps/rename-4bea47a81e61cadf: src/main.rs
+
+/home/merlin/github/me/rename/target/debug/deps/rename-4bea47a81e61cadf.d: src/main.rs
+
+src/main.rs:
---
+++ rename/target/debug/rename.d
@@ -0,0 +1,1 @@
+/home/merlin/github/me/rename/target/debug/rename: /home/merlin/github/me/rename/src/main.rs
